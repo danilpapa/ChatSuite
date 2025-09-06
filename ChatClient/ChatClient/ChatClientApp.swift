a5a16bb164365aa6861c978e7fb1c439ce1c7ddf
setuper <zabinskiy.de@finist-soft.ru>
2025-09-06 20:13:45 +0300

    initial commit

---
+++ ChatClient/ChatClient/ChatClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatClientApp.swift
+//  ChatClient
+//
+//  Created by setuper on 06.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
