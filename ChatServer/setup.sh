da1db52a604506dbc18cedd90828a29df6d0a034
danilpapa <rexportydf@mail.ru>
2025-10-19 14:59:12 +0300

    attemp adding osascript

--- ChatServer/setup.sh
+++ ChatServer/setup.sh
@@ -6,4 +6,7 @@ while ! docker info > /dev/null 2>&1; do
     sleep 1
 done
 
+# TODO: Upd osapplscript
+osascript -e 'tell application "Docker" to set miniaturized of every window to true'
+
 docker-compose up db
